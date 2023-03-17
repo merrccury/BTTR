fa576a0a700c96414b458a1dd04fb80ce867921c
merrc <merrccury@gmail.com>
2023-03-17 09:27:43 +0300

    Updated run script

--- run.sh
+++ run.sh
@@ -1,14 +1,11 @@
+#!/bin/bash
 
-wget https://repo.anaconda.com/miniconda/Miniconda3-py37_4.12.0-Linux-x86_64.sh
-chmod +x Miniconda3-py37_4.12.0-Linux-x86_64.sh
-bash ./Miniconda3-py37_4.12.0-Linux-x86_64.sh -b -f -p /usr/local/
-
-conda create -y -n bttr python=3.7
-conda activate bttr
-conda install --yes -c pytorch pytorch=1.7.0 torchvision cudatoolkit
-pip install -e .
-
-pip uninstall  torchmetrics==0.11.0
-pip install  torchmetrics==0.5
-
-python train.py --config config.yaml
+wget https://repo.anaconda.com/miniconda/Miniconda3-py37_4.12.0-Linux-x86_64.sh &&
+chmod +x Miniconda3-py37_4.12.0-Linux-x86_64.sh &&
+bash ./Miniconda3-py37_4.12.0-Linux-x86_64.sh -b -f -p /usr/local/ &&
+conda create -y -n bttr python=3.7 &&
+source activate bttr &&
+conda install --yes -c pytorch pytorch=1.7.0 torchvision cudatoolkit &&
+pip install -e . &&
+pip uninstall torchmetrics==0.11.0 &&
+pip install torchmetrics==0.5
